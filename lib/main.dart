267e2aa3be4521d050eaa2cbdcdb545f3d4073c4
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-09-19 08:37:12 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(),
+    return MaterialApp(
+      home: Container(
+        child: Column(
+          children: [Text("Data"),Text("data")],
+        ),
+      ),
     );
   }
 }
