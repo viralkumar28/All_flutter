c5c77b38970a586c4f9ab833eff7a4c519efa18c
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-11-27 11:59:46 +0530

    Flutter Basics

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,31 @@ void main(){
       appBar: AppBar(
         title: Text('New Application'),
         centerTitle: true,
+        backgroundColor: Colors.redAccent[100],
       ),
       body: Center(
-        child: Text('Hello World'),
+        child: Text('Hello World',
+        style: TextStyle(
+         fontSize: 20,
+         fontWeight: FontWeight.bold,
+         letterSpacing: 2,
+         color: Colors.pinkAccent[100],
+        ),
+        ),
+
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
         child: Icon(Icons.contact_mail),
+        backgroundColor: Colors.redAccent[100],
       ),
     ),
     )
   );
+}
+class test extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Container();
+}
 }
 
